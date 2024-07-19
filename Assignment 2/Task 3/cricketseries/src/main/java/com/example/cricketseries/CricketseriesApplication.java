1a4c9d2845812324f8058f5498af5eda99165ef4
shubham hingmire <130547918+shubhamhingmire@users.noreply.github.com>
2024-07-19 07:02:40 +0530

    Update CricketseriesApplication.java

--- Assignment 2/Task 3/cricketseries/src/main/java/com/example/cricketseries/CricketseriesApplication.java
+++ Assignment 2/Task 3/cricketseries/src/main/java/com/example/cricketseries/CricketseriesApplication.java
@@ -1,13 +1,18 @@
 package com.example.cricketseries;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CricketseriesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CricketseriesApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(CricketseriesApplication.class);
 
+    public static void main(String[] args) {
+        logger.info("Starting Cricket Series Application...");
+        SpringApplication.run(CricketseriesApplication.class, args);
+        logger.info("Cricket Series Application Started Successfully.");
+    }
 }
