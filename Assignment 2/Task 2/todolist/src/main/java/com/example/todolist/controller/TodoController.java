1c317cc0a8b60a77794ae432c187e41ccff86cac
shubham hingmire <130547918+shubhamhingmire@users.noreply.github.com>
2024-07-19 08:46:36 +0530

    Update TodoController.java

--- Assignment 2/Task 2/todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ Assignment 2/Task 2/todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,29 +15,36 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+	
+    private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
+
 
     @Autowired
     private TodoRepository todoRepository;
 
     @GetMapping
     public List<Todo> getAllTodos() {
+    	logger.info("Received request to fetch all list.");
         return todoRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+    	logger.info("Received request to fetch list with id : {} ",id);
         Optional<Todo> todo = todoRepository.findById(id);
         return todo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+    	logger.info("Received request to save new list");
         Todo savedTodo = todoRepository.save(todo);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTodo);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+    	logger.info("Received request to update the list for id : {} ",id);
         if (!todoRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
@@ -46,6 +55,7 @@ public class TodoController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+    	logger.info("Received request to delete the list for id : {}",id);
         if (!todoRepository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
